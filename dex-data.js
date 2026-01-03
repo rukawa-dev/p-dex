c15e6463391f906b51f6fa7065ddc7d4059cc68e
강석민 <rukawa8188@gmail.com>
2026-01-03 23:56:48 +0900

    ㅇㅇㅇ

--- dex-data.js
+++ dex-data.js
@@ -1,7 +1,72 @@
 const pokemonData = [
   {
-    "id": 1,
-    "name": "이상해씨",
+    "id": 61,
+    "name": "슈륙챙이",
+    "types": [
+      "water"
+    ],
+    "evolution": "발챙이 Lv.25"
+  },
+  {
+    "id": 62,
+    "name": "강챙이",
+    "types": [
+      "water",
+      "fighting"
+    ],
+    "evolution": "슈륙챙이 아이템 사용"
+  },
+  {
+    "id": 63,
+    "name": "캐이시",
+    "types": [
+      "psychic"
+    ],
+    "evolution": "기본"
+  },
+  {
+    "id": 64,
+    "name": "윤겔라",
+    "types": [
+      "psychic"
+    ],
+    "evolution": "캐이시 Lv.16"
+  },
+  {
+    "id": 65,
+    "name": "후딘",
+    "types": [
+      "psychic"
+    ],
+    "evolution": "윤겔라 통신교환"
+  },
+  {
+    "id": 66,
+    "name": "알통몬",
+    "types": [
+      "fighting"
+    ],
+    "evolution": "기본"
+  },
+  {
+    "id": 67,
+    "name": "근육몬",
+    "types": [
+      "fighting"
+    ],
+    "evolution": "알통몬 Lv.28"
+  },
+  {
+    "id": 68,
+    "name": "괴력몬",
+    "types": [
+      "fighting"
+    ],
+    "evolution": "근육몬 통신교환"
+  },
+  {
+    "id": 69,
+    "name": "모다피",
     "types": [
       "grass",
       "poison"
@@ -9,21 +74,12 @@ const pokemonData = [
     "evolution": "기본"
   },
   {
-    "id": 2,
-    "name": "이상해풀",
+    "id": 70,
+    "name": "우츠동",
     "types": [
       "grass",
       "poison"
     ],
-    "evolution": "이상해씨 Lv.16"
-  },
-  {
-    "id": 3,
-    "name": "이상해꽃",
-    "types": [
-      "grass",
-      "poison"
-    ],
-    "evolution": "이상해풀 Lv.32"
+    "evolution": "모다피 Lv.21"
   }
 ];
